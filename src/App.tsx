3354b095cda0cc01fa51a0443ed8761f45125186
YeEun Kim <kye1115z@naver.com>
2026-01-14 16:48:12 +0900

    fix: 주석 해제

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import ProductList from "./pages/ProductList/ProductList";
-// import Home from "./pages/Home/Home";
-
-// import ProductDetail from "./pages/ProductDetail/ProductDetail";
-// import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
+import ProductDetail from "./pages/ProductDetail/ProductDetail";
+import Cart from "./pages/Cart/Cart";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          {/* <Route index element={<Home />} /> */}
+          <Route index element={<Home />} />
           <Route path="products" element={<ProductList />} />
-          {/* <Route path="products/:id" element={<ProductDetail />} />
-          <Route path="cart" element={<Cart />} /> */}
+          <Route path="products/:id" element={<ProductDetail />} />
+          <Route path="cart" element={<Cart />} />
         </Route>
       </Routes>
     </BrowserRouter>
