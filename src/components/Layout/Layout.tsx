2075b2501bcc146787e1c6b317f72070fe03b95c
kimtaehyung <kth14200@gmail.com>
2026-01-14 16:41:01 +0900

    layout Header export 추가, products.ts 이미지 오류나서 수정,ProductDetail.tsx 생성

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,17 @@
 import { Outlet } from 'react-router-dom';
-import Header from './Header';
+
 import Footer from './Footer';
 import styles from './Layout.module.css';
+import Header from './Header';
 function Layout() {
-return (
-<div className={styles.layout}>
-<Header />
-<main className={styles.main}>
-<Outlet />
-</main>
-<Footer />
-</div>
-);
+    return (
+        <div className={styles.layout}>
+            <Header />
+            <main className={styles.main}>
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    );
 }
 export default Layout;
